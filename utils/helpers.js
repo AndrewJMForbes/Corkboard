7671daac2b8ffc1fa0054064781908ead0bf514b
Danny Wittig <wittigdaniel.g@gmail.com>
2023-07-16 20:20:43 -0500

    browse events population

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,13 @@
 module.exports = {
+  format_time: (date) => {
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return date.toLocaleDateString("en-US", options);
+  },
   get_emoji: () => {
     const randomNum = Math.random();
     let book = "📗";
