184bcd3d81967ac079081686e9ede7f4d10a7740
teylorsmith34 <teylorsmith34@gmail.com>
2023-07-17 17:37:15 -0400

    Added calendar.js, Modified a couple other files.

--- controllers/api/newEventRoutes.js
+++ controllers/api/newEventRoutes.js
@@ -1,19 +1,20 @@
-const router = require('express').Router();
-const { Event } = require('../../models');
+const router = require("express").Router();
+const { Event } = require("../../models");
 
-router.post('/', async (req, res) => {
-    
-    try {
-        const newEvent = await Event.create({
-            eventName: req.body.eventName,
-            eventDescription: req.body.eventDescription,
-            eventDate: req.body.eventDate,
-            eventTime: req.body.eventTime,
-            eventLocation: req.body.eventLocation
-        });
-        res.status(200).json(newEvent);
-    } catch (err) {
-        res.status(400).json('Could not post new event. Please try again later!')
-        return;
-    };
+router.post("/", async (req, res) => {
+  try {
+    const newEvent = await Event.create({
+      eventName: req.body.eventName,
+      eventDescription: req.body.eventDescription,
+      eventDate: req.body.eventDate,
+      eventTime: req.body.eventTime,
+      eventLocation: req.body.eventLocation,
+    });
+    res.status(200).json(newEvent);
+  } catch (err) {
+    res.status(400).json("Could not post new event. Please try again later!");
+    return;
+  }
 });
+
+module.exports = router;
